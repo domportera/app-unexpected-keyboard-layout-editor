6f00b3f4e66083a6345fd40101ac1a5fde5f1a45
dom <domportera@gmail.com>
2025-08-02 08:21:06 -0400

    attempt 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import { render } from "preact";
 import { App } from "./App";
 
+// Import Bootstrap JS so it is bundled by Vite
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 render(<App />, document.body);
